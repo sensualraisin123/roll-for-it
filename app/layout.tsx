43487a60a0a652fdc960effd7855bffa883823f7
Gabe <gabrielmrouse@gmail.com>
2026-05-24 11:32:56 -0700

    add app icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Roll For It 🎲',
   description: 'Let the dice decide.',
+  icons: {
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({
